848ff5d76c820694072681690eb02e36ff25afad
Swizec Teller <swizec@swizec.com>
2015-06-08 01:16:46 -0700

    Change json url to relative

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ const React = require('react'),
       ApplicantsChart = require('./ApplicantsChart');
 
 React.render(
-    <ApplicantsChart url="/bigscreen.json" />,
+    <ApplicantsChart url="bigscreen.json" />,
     document.querySelectorAll('.container')[0]
 );
