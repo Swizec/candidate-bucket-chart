42272f9d358c92e0605d1f2239368a36eff90d33
Swizec Teller <swizec@swizec.com>
2015-06-15 23:51:46 -0700

    Decrease size of packaged JS
    Moving d3 and React to externals reduces file size from 1.5MB to 469kB

--- webpack.config.js
+++ webpack.config.js
@@ -29,5 +29,10 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
+    externals: {
+        "react": "React",
+        "react/addons": "React",
+        "d3": "d3"
+    },
     devtool: 'eval-source-map'
 };
